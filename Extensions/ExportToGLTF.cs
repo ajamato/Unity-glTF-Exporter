7af4d5b605534e59361813077ab4c289e87dd800
Alex Amato <ajamato@ajamato-macbookpro.roam.corp.google.com>
2018-03-02 15:24:57 -0800

    Done

--- Extensions/ExportToGLTF.cs
+++ Extensions/ExportToGLTF.cs
@@ -1,33 +1,17 @@
 #if UNITY_EDITOR
+using System;
 using UnityEngine;
 using UnityEditor;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using SimpleJSON;
-using System.Runtime.Serialization.Formatters.Binary;
-using System;
-using UnityEditor.SceneManagement;
-using UnityEngine;
-using UnityEditor;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.IO;
-using System.IO.Compression;
-using System.Text;
-using System.Reflection;
-using Ionic.Zip;
 
 public class ExportToGLTF : EditorWindow
 {
-    GameObject exporterGo;
-    SceneToGlTFWiz exporter;
-    string path = "";
+	GameObject exporterGo;
+	SceneToGlTFWiz exporter;
+	string path = "";
 
-    [MenuItem("Tools/Export to GLTF")]
-    static void Export()
-    {
+	[MenuItem("Tools/Export to GLTF")]
+	static void Export()
+	{
 #if UNITY_STANDALONE_WIN || UNITY_STANDALONE_OSX // edit: added Platform Dependent Compilation - win or osx standalone
 		ExportToGLTF window = (ExportToGLTF)EditorWindow.GetWindow(typeof(ExportToGLTF));
 		window.titleContent.text = "Export To GLTF";
@@ -35,40 +19,40 @@ public class ExportToGLTF : EditorWindow
 #else // and error dialog if not standalone
 		EditorUtility.DisplayDialog("Error", "Your build target must be set to standalone", "Okay");
 #endif
-    }
+	}
 
-    void Awake() {
-        exporterGo = new GameObject("Exporter");
-        exporter = exporterGo.AddComponent<SceneToGlTFWiz>();
-    }
+	void Awake() {
+		exporterGo = new GameObject("Exporter");
+		exporter = exporterGo.AddComponent<SceneToGlTFWiz>();
+	}
 
-    void OnGUI()
-    {
-      path = EditorGUILayout.TextField("Output File", path);
+	void OnGUI()
+	{
+		path = EditorGUILayout.TextField("Output File", path);
 
-      GUILayout.BeginHorizontal("buttons");
-      if (GUILayout.Button("Select Output File", GUILayout.Width(150), GUILayout.Height(40))) {
-        // Hack to deal with the fact that its implemented
-        // in a Coroutine.
-        path = EditorUtility.SaveFilePanel("glTF Export Filename", "", "", "zip");
-      }
+		GUILayout.BeginHorizontal();
+		if (GUILayout.Button("Select Output File", GUILayout.Width(150), GUILayout.Height(40))) {
+			// Hack to deal with the fact that its implemented
+			// in a Coroutine.
+			path = EditorUtility.SaveFilePanel("glTF Export Filename", "", "", "zip");
+		}
 
-      EditorGUI.BeginDisabledGroup(path == String.Empty);
-      if (GUILayout.Button("Convert", GUILayout.Width(150), GUILayout.Height(40))) {
-        exporter.ExportCoroutine(path, null, true, true, false, true);
-      }
-      EditorGUI.EndDisabledGroup();
-      GUILayout.EndHorizontal();
-    }
+		EditorGUI.BeginDisabledGroup(path == String.Empty);
+		if (GUILayout.Button("Convert", GUILayout.Width(150), GUILayout.Height(40))) {
+			exporter.ExportCoroutine(path, null, true, true, false, true);
+		}
+		EditorGUI.EndDisabledGroup();
+		GUILayout.EndHorizontal();
+	}
 
-    void OnDestroy()
+	void OnDestroy()
 	  {
 		  if (exporterGo)
 		  {
 			  DestroyImmediate(exporterGo);
 			  exporter = null;
 		  }
-    }
+	}
 }
 
 #endif
